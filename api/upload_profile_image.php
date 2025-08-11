dc8abbcbc21ef215226f6bfabb6e9c17c99a8a47
Siboniso <sibandzesiboniso0@gmail.com>
2025-08-11 14:08:52 +0200

    Add CORS headers to upload_profile_image.php for cross-origin requests

--- api/upload_profile_image.php
+++ api/upload_profile_image.php
@@ -1,4 +1,12 @@
 <?php
+// CORS headers for cross-origin requests
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // api/upload_profile_image.php
 require_once 'config.php';
 header('Content-Type: application/json');
